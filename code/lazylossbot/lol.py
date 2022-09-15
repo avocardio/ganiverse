be980a95d9a669610fb3f5b0b767564e82e72e1f
avocardio <mkalcher@uni-osnabrueck.de>
2022-09-14 20:16:12 -0500

    web changes for deployment

--- code/lazylossbot/lol.py
+++
@@ -1,4 +0,0 @@
-
-from training_bot import send_text
-
-send_text("Joa glaube die sehen eigentlich schon gut aus...")
--- code/lazylossbot/training_bot_test.py
+++
@@ -1,7 +0,0 @@
-
-from training_bot import from_txt, to_txt, automated_update, clear_txt
-import random
-
-#to_txt(epoch = 4, loss = 5, acc = 6)
-
-clear_txt()
